ee358738c3cefe03d869e770b5e37a671cd1569f
xiaowan2016 <17775728499@163.com>
2017-11-17 22:28:05 +0800

    feat: 暂时不存储session到数据库

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ app.use(session({
   cookie: {
     maxAge: config.session.maxAge// 过期时间，过期后 cookie 中的 session id 自动删除
   },
-  store: new MongoStore({// 将 session 存储到 mongodb
-    url: config.mongodb// mongodb 地址
-  })
+  // store: new MongoStore({// 将 session 存储到 mongodb
+  //   url: config.mongodb// mongodb 地址
+  // })
 }))
 // flash 中间件，用来显示通知
 app.use(flash())
--- middlewares/check.js
+++ middlewares/check.js
@@ -8,7 +8,6 @@ module.exports = {
   },
 
   checkNotLogin: function checkNotLogin (req, res, next) {
-    console.log(req.session, '<<<<<<<<<<<<<<<<<<<')
     if (req.session.user) {
       req.flash('error', '已登录')
       return res.redirect('back')// 返回之前的页面
